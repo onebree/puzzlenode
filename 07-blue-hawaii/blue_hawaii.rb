f83ba74d52af8dbe106e7da19f68200e6508737b
Hunter Stevens <onebree@gmail.com>
2016-05-03 14:57:22 -0400

    Raw solution for puzzle

--- 07-blue-hawaii/blue_hawaii.rb
+++ 07-blue-hawaii/blue_hawaii.rb
@@ -3,26 +3,35 @@ require "date"
 
 SALES_TAX = 1.0411416
 
-def to_float(input)
-  input ? input[1..-1].to_f : 0.0
+def to_f(input)
+  input[1..-1].to_f
+end
+
+def to_date(date, year)
+  month, day = date.split("-").map(&:to_i)
+  Date.new(year, month, day)
 end
 
 def seasons_hash(seasons)
-  seasons_new = {
-    winter: seasons[0]["one"],
-    summer: seasons[1]["two"]
-  }
-
-  seasons_new[:winter]["rate"] = to_float(seasons_new[:winter]["rate"])
-  seasons_new[:summer]["rate"] = to_float(seasons_new[:summer]["rate"])
+  seasons_new = {}
+  seasons.each { |season| seasons_new.merge!(season) }
+  seasons_new.each do |_, season|
+    season["range"] =
+      season_start = to_date(season["start"], @start.year
+      if season["start"]  < season["end"]
+        (season_start..to_date(season["end"], @start.year))
+      else
+        (season_start..to_date(season["end"], @start.year.next))
+    end
+  end
   seasons_new
 end
 
-input = File.read("sample_input.txt")
-vacation_rentals = JSON.parse File.read("sample_vacation_rentals.json")
-output = File.open("sample_output.txt", "w")
+input  = File.read("input.txt")
+output = File.open("output.txt", "w")
+vacation_rentals = JSON.parse File.read("vacation_rentals.json")
 
-start, finish = input.split(" - ").map { |x| Date.parse(x) }
+@start, @finish = input.split(" - ").map { |day| Date.parse(day) }
 
 vacation_rentals.each do |rental|
   name         = rental["name"]
@@ -32,28 +41,21 @@ vacation_rentals.each do |rental|
 
   subtotal = 
     if rate
-      (start...finish).to_a.length * to_float(rate)
+      (@start...@finish).to_a.length * to_f(rate)
     else
       seasons = seasons_hash(seasons)
-      winter = (seasons[:winter]["start"]..seasons[:winter]["end"])
-      summer = (seasons[:summer]["start"]..seasons[:summer]["end"])
-      
       season_rate = 0.0
-
-      (start...finish).each do |day|
-        if winter.include? day.strftime("%m-%d")
-          season_rate += seasons[:winter]["rate"]
-        else
-          season_rate += seasons[:summer]["rate"]
+      (@start...@finish).each do |day|
+        seasons.each do |_, values|
+          season_rate += to_f(values["rate"]) if values["range"].include?(day)
         end
       end
       season_rate
   end
   
-  subtotal += to_float(cleaning_fee)
-
+  subtotal += to_f(cleaning_fee) if cleaning_fee
   total = subtotal * SALES_TAX
-  total = total.round(2)
+  total = "%.02f" % total
 
   output.puts "#{name}: $#{total}"
 end
