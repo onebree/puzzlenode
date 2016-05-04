7e438a3ee4527a4b179b0edfbd7dc323b77b386f
Hunter Stevens <onebree@gmail.com>
2016-05-04 14:20:57 -0400

    Working solution for sample data

--- 14-six-degrees-of-separation/six_degrees_of_separation.rb
+++ 14-six-degrees-of-separation/six_degrees_of_separation.rb
@@ -1,35 +1,43 @@
 tweets = File.readlines("sample_input.txt")
 
-results = Hash.new { |h,k| h[k] = [] }
+@results = Hash.new { |h,k| h[k] = [] }
 
 tweets.each do |tweet|
   owner    = tweet.scan(/\A(\w+):/).flatten[0]
   mentions = tweet.scan(/@(\w+)/).flatten
-  results[owner] = results[owner] + mentions
+  @results[owner] = @results[owner] + mentions
 end
 
-results.each do |owner, mentions|
-  results.each do |o, m|
+@results.each do |owner, mentions|
+  @results.each do |o, m|
     next if owner == o
     unless mentions.include?(o) && m.include?(owner)
-      results[owner].delete(o)
+      @results[owner].delete(o)
     end
   end
 end
 
-connections = {}
+@connections = {}
 
-results.each do |owner, mentions|
-  connections[owner] = { first: mentions, second: [] }
-  mentions.each do |mention|
-    results[mention].each do |x|
-      next if x == owner || mentions.include?(x)
-      connections[owner][:second] << x
+@results.each { |o,m| @connections[o] = { "0" => m.sort } }
+
+(1..@results.length).each do |degree|
+  @results.each do |owner, mentions|
+    @connections[owner][degree.to_s] = []
+
+    @connections[owner][degree.pred.to_s].each do |mention|
+      @results[mention].each do |m|
+        next if owner == m
+        @connections[owner][degree.to_s] << m
+      end
+    end
+
+    @connections[owner][degree.to_s].uniq!
+
+    degree.pred.downto(0) do |n|
+      @connections[owner][degree.to_s] = @connections[owner][degree.to_s] - @connections[owner][n.to_s]
     end
   end
-
-  connections[owner][:first].uniq!
-  connections[owner][:second].uniq!
 end
 
-connections.each { |x,y| puts x, y, "\n" }
+@connections.each { |x,y| puts x, y, "\n" }
