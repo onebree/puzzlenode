a1ead76c5ae15e196d1c7432d7995f34a6825730
Hunter Stevens <onebree@gmail.com>
2016-05-12 11:07:00 -0400

    WIP branching for sol v2

--- 05-counting-cards/counting_cards_2.rb
+++ 05-counting-cards/counting_cards_2.rb
@@ -1,3 +1,24 @@
+class Round
+  attr_accessor :turns
+
+  def initialize
+    @turns = []
+  end
+
+  def <<(turn)
+    @turns << turn
+  end
+end
+
+class Turn
+  attr_accessor :player, :moves
+
+  def initialize(player, moves)
+    @player = player
+    @moves = moves
+  end
+end
+
 def action(move)
   move[/([-+][0-9JQKA?]{1,2}[CDHS?])/]
 end
@@ -15,73 +36,44 @@ def other_player(move)
 end
 
 def valid_signal?(signal, unknowns)
-  # If Lil has 3 unknowns, but the signal only fills in 2, then it must be false
   return false if signal.length != unknowns.length
 
-  # If the same action appears multiple times in a signal
   actions = signal.map { |x| action(x) }
   return false if actions != actions.uniq
 
-  # Check each move within the signal individually
   signal.each_with_index do |move, i|
-    unknown = unknowns[i]
-
-    action = action(move)
     sign = sign(move)
-    card = card(move)
     other_player = other_player(move)
-
-    # if playing a card that's already discarded
-    if @discarded.include?(card)
-      return false
-
-    # ensures the order and sign/other of signals match
-    elsif sign != sign(unknown) || other_player != other_player(unknown)
-      return false
-
-    # if passing a card possessed by someone else
-    elsif sign == "-" && @branch.values_at("Shady", "Danny", "Rocky").include?(card)
-      return false
-
-    # if drawing a card that Lil already has
-    elsif sign == "+" && @branch["Lil"].include?(card)
-      return false
-    end
+    unknown = unknowns[i]
+    return false if sign != sign(unknown) || other_player != other_player(unknown)
   end
 
   return true
 end
 
-@hands = {
-  "Lil"   => %w( 5C 2H 8H 6D ),
-  "Shady" => %w( QH AC 7C 2D 8C 3S ),
-  "Rocky" => %w( KS ),
-  "Danny" => %w( 4H ),
-}
+def valid_move?(branch, player, move)
+  card = card(move)
+  sign = sign(move)
+  other_players = branch.keys.delete(player)
 
-@discarded = %w( 4D 7D JS 6S 6H 2C 5D 3C )
+  return true if card == "??"
 
-signals = [
-  ["+8H:Shady", "-2H:Danny", "+JD",  "+2D"],
-  ["+8C:Shady", "-8C:Danny", "+JD",  "+4S"],  # correct
-  ["+QH:Shady", "-2H:Danny", "+7D",  "+AS"],
-  ["+AC:Shady", "-8H:Rocky", "+AS",  "+8D"],
-  ["+8C:Shady", "-2H:Danny", "+10H", "+9H", "+4C"],
-  ["-8H:Danny", "+8C:Shady", "+4S",  "+AS"],
-]
+  if branch["discard"].include?(card)
+    return false
+  elsif sign == "+" && branch[player].include?(card)
+    return false
+  elsif sign == "-" && branch.values_at(*other_players).include?(card)
+    return false
+  end
 
-turn = ["+??:Shady", "-6D:discard", "-??:Danny", "+??", "+??"]
+  return true
+end
 
-unknowns = turn.select { |x| x.include?("??") }
-
-valid_signals = signals.select { |x| valid_signal?(x, unknowns) }
-
-valid_signals.each do |signal|
-  @branch = Marshal.load(Marshal.dump(@hands))
-  # Replace unknowns with hints from signal
+def parse_signal_for_lil(signal, turn)
+  unknowns = turn.moves.select { |x| x.include?("??") }
   new_moves = []
   count = 0
-  turn.each do |move|
+  turn.moves.each do |move|
     if unknowns.include?(move)
       new_moves.push(signal[count])
       count += 1
@@ -91,20 +83,99 @@ valid_signals.each do |signal|
   end
 
   turn = new_moves
+end
 
-  # Process Lil's hand with filled in moves
-  turn.each do |move|
-    action = action(move)
-    card = card(move)
-    other_player = other_player(move)
-
-    if action.start_with? "+"
-      @branch["Lil"].push(card)
-    elsif action.start_with? "-"
-      @branch["Lil"].delete(card)
-      @discarded.push(card) unless other_player
+def valid_turn?(turn)
+  turn.moves.each do |moves|
+    moves.each do |move|
+      return false unless valid_move?(turn.player, move)
     end
   end
 
-  puts @branch["Lil"].join(" ")
+  return true
+end
+
+def modify_hand(branch, player, move)
+  action = action(move)
+  card = card(move)
+  other_player = other_player(move)
+
+  unless valid_move?(branch, player, move)
+    puts player, move.inspect
+    raise
+  end
+
+  if action.start_with? "+"
+    branch[player].push(card)
+  elsif action.start_with? "-"
+    # todo - if player previously had only ??, but discards, then remove a ??
+    branch[player].delete(card)
+    branch["discard"].push(card) unless other_player
+  end
+end
+
+def new_branch(old)
+  Marshal.load(Marshal.dump(old))
+end
+
+@hands = {
+  "Lil"     => [],
+  "Shady"   => [],
+  "Rocky"   => [],
+  "Danny"   => [],
+  "discard" => []
+}
+
+@signals = Hash.new { |h,k| h[k] = [] }
+@rounds  = []
+
+
+File.foreach("SAMPLE_INPUT.txt") do |turn|
+  player = turn[/\A(\w+|\*) /, 1]
+  moves  = turn.scan /[-+][0-9JQKA\?]{1,2}[CDHS\?]:*\w*/
+
+  if player == "*"
+    index = @rounds.length.pred
+    @signals[index].push moves
+  else
+    turn = Turn.new(player, moves)
+
+    if player == "Shady"
+      @round   = Round.new
+      @rounds << @round
+    end
+
+    @round << turn
+  end
+end
+
+# round 0 is always valid
+@rounds.first.turns.each do |turn|
+  turn.moves.each do |move|
+    modify_hand(@hands, turn.player, move)
+  end
+end
+
+# Start the first branch from round 1
+@rounds[1..-1].each do |round|
+  old_branch = @branch ? @branch : @hands
+
+  index = @rounds.index(round)
+  puts "ROUND: #{index}"
+
+  round.turns.each do |turn|
+    if turn.player == "Lil"
+      unknowns = turn.moves.select { |x| x.include?("??") }
+      valid_signals = @signals[index].select { |x| valid_signal?(x, unknowns) }
+      @branch = new_branch(old_branch) if valid_signals.length > 1
+
+      # From here, move into possibilities
+      @current_signal = valid_signals.first
+      turn.moves = parse_signal_for_lil(@current_signal, turn)
+    end
+
+    turn.moves.each do |move|
+      modify_hand(@hands, turn.player, move)
+    end
+  end
 end
